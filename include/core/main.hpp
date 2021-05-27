b7f87ed5998e5d5d2ebc28fef20bc75705601c39
Vinicius Assis <viniciusassis2012@gmail.com>
2021-05-26 22:28:50 -0300

    - Initial commit

---
+++ include/core/main.hpp
@@ -0,0 +1,29 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+// Bibliotecas Principais
+#include <glad/glad.h>
+#include <SDL2/SDL.h>
+
+// Bibliotecas Auxiliares
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+// Constantes
+#define SCREEN_WIDTH    640
+#define SCREEN_HEIGHT   480
+
+struct ParsedShader;
+
+// Definicao de Funcoes
+void setupWindow(const char*);
+void finishError(std::string);
+void mainLoop();
+ParsedShader parseShader(const std::string&);
+unsigned int createShader(const std::string&, const std::string&);
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,173 @@
+#include <core/main.hpp>
+
+    SDL_Window      *window;
+    SDL_GLContext    context;
+
+struct ParsedShader
+{
+    std::string VertexShader;
+    std::string FragmentShader;
+};
+ 
+int main(int argc, char** argv) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        finishError("Nao foi possivel inicializar o SDL");
+
+    setupWindow("Main Window");
+    mainLoop();
+
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
+void setupWindow(const char *title){
+    window = SDL_CreateWindow(
+        title,
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+    );
+
+    context = SDL_GL_CreateContext(window);
+    gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
+    if (!window || !context)
+        finishError("Nao foi possivel inicializar a Janela");
+}
+
+void mainLoop() {
+    bool isRunning = true;
+    SDL_Event e;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    ParsedShader shaders = parseShader("resources/shaders/base.shader");
+
+    unsigned int shader = createShader(shaders.VertexShader, shaders.FragmentShader);
+
+    glUseProgram(shader);
+    
+    while(isRunning){
+        //Handle events on queue
+        while( SDL_PollEvent(&e) != 0 ){
+            switch (e.type){
+
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            
+            default:
+                break;
+            }
+        }
+
+        //Clear screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Update screen
+        //MODERN OPENGL
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+        // OLD OPENGL
+        // glBegin(GL_TRIANGLES);
+        // glColor3f (1, 1, 0);
+        // glVertex2f(-0.5f, -0.5f);
+        // glColor3f (0, 1, 1);
+        // glVertex2f( 0.0f,  0.5f);
+        // glColor3f (1, 0, 1);
+        // glVertex2f( 0.5f, -0.5f);
+        // glEnd();
+
+        // Exchange frame (?) buffer (see Vulkan tutorial for what this is)
+        SDL_GL_SwapWindow(window);
+
+        //Wait to next frame
+    }
+}
+
+void finishError(std::string err_msg) {
+    std::cout << "Ocorreu um problema durante a execucao do programa: " << err_msg << std::endl;
+    fflush(stdout);
+    //SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", err_msg.c_str(), NULL);
+    SDL_Quit();
+}
+
+ParsedShader parseShader(const std::string& filePath) {
+    std::ifstream stream(filePath);
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            } else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        } else {
+            ss[(int)type] << line << '\n';
+        }
+    }
+    return { ss[0].str(), ss[1].str() };
+}
+
+unsigned int compileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int lenght;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &lenght);
+        char* message = (char*)alloca(sizeof(char) * lenght);
+        glGetShaderInfoLog(id, lenght, &lenght, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << " shader: " << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
