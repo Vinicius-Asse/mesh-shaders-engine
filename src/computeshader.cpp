fe467f2d098789d1dc09f08c79cffd5b6c059346
Vinicius Assis <viniciusassis2012@gmail.com>
2021-10-27 15:42:28 -0300

    - Implementação de ComputeShader.cpp

---
+++ src/computeshader.cpp
@@ -0,0 +1,79 @@
+#include<core/computeshader.hpp>
+
+ComputeShader::ComputeShader(const std::string& shaderPath) {
+    std::string shaderStr = parseShader(shaderPath);
+    uId = createShader(shaderStr);
+    std::cout << "[" << std::to_string(uId) << "] * Compute Shader Program Created * " << std::endl;
+}
+
+ComputeShader::~ComputeShader() {
+    std::cout << "[" << std::to_string(uId) << "] ~ Deleting Shader Data" << std::endl;
+    glDeleteProgram(uId);
+}
+
+void ComputeShader::enable() {
+    glUseProgram(uId);
+}
+
+void ComputeShader::disable() {
+    glUseProgram(0);
+}
+
+std::string ComputeShader::parseShader(const std::string& filePath) {
+    std::ifstream stream(filePath);
+
+    std::string line;
+    std::stringstream ss;
+    while (getline(stream, line)) {
+        std::regex includeRegex("#include\\s+\"(.*)\"");
+
+        if (std::regex_search(line, includeRegex)) {
+            std::smatch sm;
+            std::string includePath = "";
+            if (std::regex_match(line, sm, includeRegex)) {
+                includePath = sm[1].str();
+            }
+            ss << parseShader(includePath) << "\n";
+        } else {
+            ss << line << "\n";
+        }
+    }
+
+    std::cout << ss.str() << std::endl;
+
+    return ss.str();
+}
+
+unsigned int ComputeShader::createShader(std::string shaderStr) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_COMPUTE_SHADER, shaderStr);
+
+    glAttachShader(program, vs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+
+    return program;
+}
+
+unsigned int ComputeShader::compileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int lenght;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &lenght);
+        char* message = (char*)alloca(sizeof(char) * lenght);
+        glGetShaderInfoLog(id, lenght, &lenght, message);
+        std::cout << "Failed to compile compute shader: " << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
