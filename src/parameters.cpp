7a875b468bac6b74f4356ee8bb53080919db4564
Vinicius Assis <viniciusassis2012@gmail.com>
2022-02-08 23:57:01 -0300

    - Alterando parametros iniciais

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -6,8 +6,8 @@ Parameters::Parameters()
     worldBounds         = glm::vec3(10.0f, 10.0f, 10.0f);
     noiseDisplacement   = glm::vec3(0.0f , 0.0f , 0.0f);
     noiseScale          = 0.001f;
-    surfaceLevel        = 0.0f;
-    surfaceResolution   = 8.0f;
+    surfaceLevel        = -0.75f;
+    surfaceResolution   = 16.0f;
     smoothIntersect     = 2.0f;
     pointsCount         = 10;
     smooth              = false;
